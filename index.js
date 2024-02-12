c2117342fa26d01d429544b3bfce917b89073ea6
Ankit Negi <ankit@optimizory.com>
2024-02-12 22:19:55 +0530

    cron job updated for timezone

--- index.js
+++ index.js
@@ -14,19 +14,26 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-cron.schedule('15 22 * * *', async () => {
+const indianTimezone = 'Asia/Kolkata';
+cron.schedule('30 22 * * *', async () => {
   console.log("entered the process")
   await uploadToRevivingSoulz()
+}, {
+  timezone: indianTimezone
 });
 
 cron.schedule('0 11 * * *', async () => {
   await uploadToRevivingSoulz()
+}, {
+  timezone: indianTimezone
 });
 
 
 //token generators
 cron.schedule('0 0 10 * *', async () => {
   await generateFbUserToken("f4b3d6ab886f041f78d5b8cc11c0f7d5", 693336926299891, "revivingSoulz")
+}, {
+  timezone: indianTimezone
 });
 
 // Start the Express server
@@ -37,6 +44,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', require("./routes/index"));
 app.listen(PORT, () => {
+
   console.log(`Server is running`);
 });
 
