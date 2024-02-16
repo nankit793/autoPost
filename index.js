09849a3324575939d13ac97c83bf055512527905
Ankit Negi <ankit@optimizory.com>
2024-02-16 22:26:41 +0530

    removed upload codwe

--- index.js
+++ index.js
@@ -15,33 +15,33 @@ app.use(
 );
 app.use(bodyParser.json());
 
-const indianTimezone = 'Asia/Kolkata';
+// const indianTimezone = 'Asia/Kolkata';
 
-cron.schedule('0 11 * * *', async () => {
-  await uploadToRevivingSoulz()
-}, {
-  timezone: indianTimezone
-});
+// cron.schedule('0 11 * * *', async () => {
+//   await uploadToRevivingSoulz()
+// }, {
+//   timezone: indianTimezone
+// });
 
-cron.schedule('0 22 * * *', async () => {
-  await uploadToRevivingSoulz()
-}, {
-  timezone: indianTimezone
-});
+// cron.schedule('0 22 * * *', async () => {
+//   await uploadToRevivingSoulz()
+// }, {
+//   timezone: indianTimezone
+// });
 
 
 //token generators
-cron.schedule('0 0 10 * *', async () => {
-  await generateFbUserToken("f4b3d6ab886f041f78d5b8cc11c0f7d5", 693336926299891, "revivingSoulz")
-}, {
-  timezone: indianTimezone
-});
+// cron.schedule('0 0 10 * *', async () => {
+//   await generateFbUserToken("f4b3d6ab886f041f78d5b8cc11c0f7d5", 693336926299891, "revivingSoulz")
+// }, {
+//   timezone: indianTimezone
+// });
 
-cron.schedule('0 0 20 * *', async () => {
-  await generateFbUserToken("f4b3d6ab886f041f78d5b8cc11c0f7d5", 693336926299891, "revivingSoulz")
-}, {
-  timezone: indianTimezone
-});
+// cron.schedule('0 0 20 * *', async () => {
+//   await generateFbUserToken("f4b3d6ab886f041f78d5b8cc11c0f7d5", 693336926299891, "revivingSoulz")
+// }, {
+//   timezone: indianTimezone
+// });
 
 // Start the Express server
 app.get('/', (req, res) => {
