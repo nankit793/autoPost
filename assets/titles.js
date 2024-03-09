18868b306c1f23f786c000b6bb6db139736e9046
Ankit Negi <ankit@optimizory.com>
2024-03-09 20:54:10 +0530

    title and tags updated for animeto

--- assets/titles.js
+++ assets/titles.js
@@ -1,11 +1,20 @@
 const revivingSoulzTiles = [
-    "This is title", "This may be title", "this could also be title"
-]
+  "This is title",
+  "This may be title",
+  "this could also be title",
+];
 const animetoTitles = [
-    "This is title", "This may be title", "this could also be title"
-]
+  "This is one of the best scene in Anime.",
+  "This scene has saperate fan base",
+  "what do you think about this?",
+  "what would you do?",
+  "what are your thoughts?",
+  "can it be any better?",
+];
 
 const reditoTitles = [
-    "This is title", "This may be title", "this could also be title"
-]
-module.exports = { revivingSoulzTiles, animetoTitles, reditoTitles }
+  "This is title",
+  "This may be title",
+  "this could also be title",
+];
+module.exports = { revivingSoulzTiles, animetoTitles, reditoTitles };
